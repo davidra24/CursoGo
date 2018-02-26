14298f871c825ea6ef1cea4a3099e96b2f050052
David Ramirez <cdavidramirezg24@gmail.com>
2018-02-25 21:53:39 -0500

    variables enteras y flotantes

--- main.go
+++ main.go
@@ -23,6 +23,14 @@ func getName() string {
 	return name
 }
 
+func getIntegers() (int, int32, int64) {
+	return 10, 2147000000, 9031313131313131313
+}
+
+func getFloats() (float32, float64) {
+	return float32(0.1), float64(0.1)
+}
+
 func getVariables() (int, int, int) {
 	return 1, 2, 3
 }
