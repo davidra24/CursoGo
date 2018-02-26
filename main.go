c6cf0adf134674be64eefd56d5778a72971f8b46
David Ramirez <cdavidramirezg24@gmail.com>
2018-02-25 21:14:28 -0500

    Declaracion de variables y constantes

--- main.go
+++ main.go
@@ -3,9 +3,21 @@ package main
 //fmt imprime en pantalla
 import "fmt"
 
+const helloWorld string = "Hola %s %s, bienvenido al fascinante mundo de Go.\n"
+const testConst = "Test"
+
 func main() {
+	//declaracion de variables
 	var name string
+	//:= crea una variable; = asigna la variable
+	lastname := "<Modificar con mi apellido>"
+	var number = 100
+	var (
+		a = 1
+		b = 2
+		c = 3
+	)
 	fmt.Print("Escribe tu nombre: ")
-	fmt.Scanf("%s", &name)
-	fmt.Printf("Hola %s, bienvenido al mundo de GO \n", name)
+	fmt.Printf(helloWorld, name, lastname)
+	fmt.Println(number, a, b, c)
 }
